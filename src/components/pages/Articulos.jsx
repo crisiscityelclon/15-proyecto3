6e6693da42faecff6eafb769d8db9c950c234c65
Cristian Gimenez <crisiscityelclon@gmail.com>
2026-01-04 13:38:50 -0300

    201. Elimina datos de prueba y añade función para obtener artículos desde la API

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -5,44 +5,43 @@ export const Articulos = () => {
   const [articulos, setArticulos] = useState([]);
 
   useEffect(() => {
-    let data = [
-      {
-        _id: 1,
-        titulo: "Titulo 1",
-        contenido: "Contenido 1",
-      },
-      {
-        _id: 2,
-        titulo: "Titulo 2",
-        contenido: "Contenido 2",
-      },
-      {
-        _id: 3,
-        titulo: "Titulo 3",
-        contenido: "Contenido 3",
-      },
-    ];
-    setArticulos(data);
+    conseguirArticulos();
   }, []);
+
+  const conseguirArticulos = async () => {
+    const url = "http://localhost:3900/api/articulos";
+    let peticion = await fetch(url, {
+      method: "GET",
+    });
+    let datos = await peticion.json();
+
+    if (datos.status === "success") {
+      setArticulos(datos.articulos);
+    }
+  };
 
   return (
     <>
-      {articulos.map((articulo) => {
-        return (
-          <article key={articulo._id} className="articulo-item">
-            <div className="mascara">
-              <img src="https://concepto.de/wp-content/uploads/2018/09/google-inc-e1537467602232.jpg" />
-            </div>
-            <div className="datos">
-              <h3 className="title">{articulo.titulo}</h3>
-              <p className="description">{articulo.contenido}</p>
+      {articulos.length >= 1 ? (
+        articulos.map((articulo) => {
+          return (
+            <article key={articulo._id} className="articulo-item">
+              <div className="mascara">
+                <img src="https://concepto.de/wp-content/uploads/2018/09/google-inc-e1537467602232.jpg" />
+              </div>
+              <div className="datos">
+                <h3 className="title">{articulo.titulo}</h3>
+                <p className="description">{articulo.contenido}</p>
 
-              <button className="edit">Editar</button>
-              <button className="delete">Borrar</button>
-            </div>
-          </article>
-        );
-      })}
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+              </div>
+            </article>
+          );
+        })
+      ) : (
+        <h1>No hay articulos</h1>
+      )}
     </>
   );
 };
