7004ca2b1663d5be6584c5e07160ebbddd1ff29f
Cristian Gimenez <crisiscityelclon@gmail.com>
2026-01-04 12:10:58 -0300

    199. Empesando con la seccion de articulos

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export const Articulos = () => {
   return (
-    <div>Articulos</div>
-  )
-}
+    <>
+      <article className="articulo-item">
+        <div className="mascara">
+          <img src="https://concepto.de/wp-content/uploads/2018/09/google-inc-e1537467602232.jpg" />
+        </div>
+        <div className="datos">
+          <h3 className="title">Desarrollo web</h3>
+          <p className="description">victorroblesweb.es</p>
+
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </div>
+      </article>
+
+         </>
+  );
+};
