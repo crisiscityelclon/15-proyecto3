c96f3dc0bd2eecd31cedf742f7e60f3cbc834dff
Cristian Gimenez <crisiscityelclon@gmail.com>
2026-01-06 11:54:13 -0300

    204. 205. Efecto de carga Mejorando el codigo y abstrayendo funcionalidades

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -2,41 +2,32 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Global } from "../../helpers/Global";
 import { Peticion } from "../../helpers/Peticion";
+import { Listado } from "./Listado";
 
 export const Articulos = () => {
   const [articulos, setArticulos] = useState([]);
+  const [cargando, setCargando] = useState(true);
 
   useEffect(() => {
     conseguirArticulos();
   }, []);
 
   const conseguirArticulos = async () => {
-    const {datos, cargando} = await Peticion(Global.url + "articulos", "GET")
+    const { datos, cargando } = await Peticion(Global.url + "articulos", "GET");
 
     if (datos.status === "success") {
       setArticulos(datos.articulos);
     }
+
+    setCargando(false);
   };
 
   return (
     <>
-      {articulos.length >= 1 ? (
-        articulos.map((articulo) => {
-          return (
-            <article key={articulo._id} className="articulo-item">
-              <div className="mascara">
-                <img src="https://concepto.de/wp-content/uploads/2018/09/google-inc-e1537467602232.jpg" />
-              </div>
-              <div className="datos">
-                <h3 className="title">{articulo.titulo}</h3>
-                <p className="description">{articulo.contenido}</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-              </div>
-            </article>
-          );
-        })
+      {cargando ? (
+        "cargando..."
+      ) : articulos.length >= 1 ? (
+        <Listado articulos={articulos} setArticulos={setArticulos} />
       ) : (
         <h1>No hay articulos</h1>
       )}
---
+++ src/components/pages/Listado.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Listado = ({ articulos, setArticulos }) => {
+  return articulos.map((articulo) => {
+    return (
+      <article key={articulo._id} className="articulo-item">
+        <div className="mascara">
+          <img src="https://concepto.de/wp-content/uploads/2018/09/google-inc-e1537467602232.jpg" />
+        </div>
+        <div className="datos">
+          <h3 className="title">{articulo.titulo}</h3>
+          <p className="description">{articulo.contenido}</p>
+
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </div>
+      </article>
+    );
+  });
+};
