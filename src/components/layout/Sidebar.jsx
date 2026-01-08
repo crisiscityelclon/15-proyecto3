c3ae235f48abcf5c6eb917700dd8f61a72fddbd7
Cristian Gimenez <crisiscityelclon@gmail.com>
2026-01-08 11:46:19 -0300

    215. Agregar funcionalidad de búsqueda en el Sidebar y definir ruta para la página de resultados

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,15 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Sidebar = () => {
+
+  const [buscar, setBuscar] = useState("");
+  const navegar = useNavigate();
+
+  const hecerBusqueda = (e) => {
+    e.preventDefault();
+    let mi_busqueda = e.target.search_field.value;
+    navegar ("/buscar/" + mi_busqueda, { replace: true });
+  }
   
   return (
     <aside className="lateral">
       <div className="search">
         <h3 className="title">Buscador</h3>
-        <form>
-          <input type="text" />
-          <button>Buscar</button>
+        <form onSubmit={hecerBusqueda}>
+          <input type="text" name="search_field"/>
+          <input type="submit" id="search" value="Buscar"/>
         </form>
       </div>
       
