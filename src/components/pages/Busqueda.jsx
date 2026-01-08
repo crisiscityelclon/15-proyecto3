570a9ae09056be34d12524e9a4bf19e580232b04
Cristian Gimenez <crisiscityelclon@gmail.com>
2026-01-08 14:33:42 -0300

    216. Página del buscador

--- src/components/pages/Busqueda.jsx
+++ src/components/pages/Busqueda.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Global } from "../../helpers/Global";
+import { Peticion } from "../../helpers/Peticion";
+import { Listado } from "./Listado";
+import { useParams } from "react-router-dom";
 
 export const Busqueda = () => {
+  const [articulos, setArticulos] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const params = useParams();
+
+  useEffect(() => {
+    conseguirArticulos();
+  }, []);
+
+  useEffect(() => {
+    conseguirArticulos();
+  }, [params]);
+
+  const conseguirArticulos = async () => {
+    const { datos, cargando } = await Peticion(Global.url + "buscar/" + params.busqueda, "GET");
+
+    if (datos.status === "success") {
+      setArticulos(datos.articulos);
+    }
+
+    setCargando(false);
+  };
+
   return (
-    <div>Busqueda</div>
-  )
-}
+    <>
+      {cargando ? (
+        "cargando..."
+      ) : articulos.length >= 1 ? (
+        <Listado articulos={articulos} setArticulos={setArticulos} />
+      ) : (
+        <h1>No hay articulos</h1>
+      )}
+    </>
+  );
+};
